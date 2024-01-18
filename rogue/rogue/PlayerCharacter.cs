29e96d973a56b9519dce4a8ea9724da5b2a15aec
utta <111429116+Uttamoosa@users.noreply.github.com>
2024-01-18 09:36:34 +0200

    nouuu

---
+++ rogue/rogue/PlayerCharacter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rogue
+{
+    public enum Race
+    {
+        human
+    }
+
+    public enum Class 
+    { 
+
+    }
+
+    internal class PlayerCharacter
+    {
+
+    }
+}
---
+++ rogue/rogue/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace rogue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("nojuu");
+        }
+    }
+}
